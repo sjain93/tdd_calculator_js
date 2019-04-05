b31754c20f48a93eca9c4772851ad95b2a91a15a
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-04-05 14:46:52 -0400

    all tests passed

--- calculator.js
+++ calculator.js
@@ -1,10 +1,17 @@
-function add() {
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract() {
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function sum() {
+function sum(array) {
+  let output = 0
+  for (var i = 0; i < array.length; i++) {
+    output += array[i];
+  }
+  return output;
 }
 
 module.exports = {add, subtract, sum}
